203a643a06808b684c613bafaec9e782df7cec64
BrianMohabeer <114097329+BrianMohabeer@users.noreply.github.com>
2026-04-17 15:43:19 -0400

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import * as cheerio from "cheerio"; // IMPORTANT: namespace import
 import puppeteer from "puppeteer-core";
-import chromium from "@sparticuz/chromium";
+import chromium from "chrome-aws-lambda";
 
 // Load environment variables
 dotenv.config();
